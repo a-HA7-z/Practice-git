8268108106af3c1b5dd646df35dc8f606fa49928
a-HA7-z <hosseinazizpour76@gmail.com>
2025-04-17 15:03:18 +0330

    Second Commit

--- main.cpp
+++ main.cpp
@@ -5,9 +5,15 @@ void print(){
     cout << "Hello, World!" << endl;
 }
 
+void stuNumber(){
+    cout << "4033613057" << endl;
+}
+
 int main() {
 
     print();
+
+    stuNumber();
 
     return 0;
 }
